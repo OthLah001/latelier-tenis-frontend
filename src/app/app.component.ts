c12e1157f34887ddcb709766f35e393069142b1f
OthmaneIGAO <othmane@igotanoffer.com>
2022-11-19 10:30:09 +0000

    dev: get & show players

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CustomHttpClientService } from './services/custom-http-client.service';
+import { IPlayer } from './utils/models';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'latelier-tenis-frontend';
+
+  public players: IPlayer[] = [];
+
+  constructor(
+    private customHttp: CustomHttpClientService
+  ) { }
+
+  public ngOnInit(): void {
+    this.customHttp.get('/players').subscribe(
+      players => {
+        this.players = players;
+      }
+    )
+  }
 }
