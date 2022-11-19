a4b24ccc26006ca7e1d852f3f0bd93fd9eddf63c
OthmaneIGAO <othmane@igotanoffer.com>
2022-11-19 08:43:59 +0000

    dev: Create custom http client service

---
+++ src/app/services/custom-http-client.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomHttpClientService {
+  private BASE_URL = environment.apiUrl;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  get(url: string, params = {}): Observable<any> {
+    return this.http.get(`${this.BASE_URL}${url}}`, { params }).pipe(
+      catchError(err => {
+        return throwError(err);
+      })
+    );
+  }
+}
